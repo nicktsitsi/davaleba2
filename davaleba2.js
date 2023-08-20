e1ee1843bd4a543b0b80be2ee76be94e1dba983f
nicktsitsi <137085491+nicktsitsi@users.noreply.github.com>
2023-08-20 16:43:37 +0400

    Add files via upload

---
+++ davaleba2.js
@@ -0,0 +1,21 @@
+const movieTitles = [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "The Dark Knight",
+    "Pulp Fiction",
+    "Fight Club",
+    "Inception",
+    "Forrest Gump",
+    "The Matrix",
+    "The Lord of the Rings: The Fellowship of the Ring",
+    "Star Wars: Episode IV - A New Hope"
+];
+
+movieTitles.push("Avatar", "Jurassic Park");
+movieTitles.unshift("Back to the Future", "The Lion King");
+movieTitles.shift(); 
+movieTitles.pop();
+
+
+
+console.log(movieTitles);
